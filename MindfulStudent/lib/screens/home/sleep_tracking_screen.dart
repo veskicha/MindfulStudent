e92e90dbfd2e2304a0ea4d870c56a16341a4ea61
Mike A <git@mikealmel.ooo>
2024-03-26 21:03:09 +0100

    feat(sleep-tracking): Make sleep data page responsive to login status

--- MindfulStudent/lib/screens/home/sleep_tracking_screen.dart
+++ MindfulStudent/lib/screens/home/sleep_tracking_screen.dart
@@ -1,7 +1,9 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:mindfulstudent/provider/sleep_tracking_provider.dart';
 import 'package:mindfulstudent/widgets/bottom_nav_bar.dart';
 import 'package:mindfulstudent/widgets/header_bar.dart';
+import 'package:provider/provider.dart';
 
 class SleepTrackingPage extends StatefulWidget {
   const SleepTrackingPage({super.key});
@@ -47,138 +49,153 @@ class SleepTrackingPageState extends State<SleepTrackingPage> {
         preferredSize: Size.fromHeight(80.0),
         child: HeaderBar('Sleep Tracking'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: BarChart(
-              BarChartData(
-                barGroups: chartData.indexed
-                    .map((d) => BarChartGroupData(x: d.$1, barRods: [d.$2]))
-                    .toList(),
-              ),
+      body: Consumer<SleepDataProvider>(
+          builder: (context, sleepDataProvider, child) {
+        if (sleepDataProvider.sleepData == null) {
+          return buildLoginContent(context);
+        } else {
+          return buildDataContent(context);
+        }
+      }),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget buildLoginContent(BuildContext context) {
+    return const Text("pls login! thank");
+  }
+
+  Widget buildDataContent(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: BarChart(
+            BarChartData(
+              barGroups: chartData.indexed
+                  .map((d) => BarChartGroupData(x: d.$1, barRods: [d.$2]))
+                  .toList(),
             ),
           ),
-          Container(
-            color: const Color(0xFF497077),
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Your optimal bedtime',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    '21:30 - 22:30',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle daily tips and suggestions action
-                    },
-                    child: const Text(
-                      'Daily tips and suggestions →',
-                      style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
+        ),
+        Container(
+          color: const Color(0xFF497077),
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16.0),
-                    color: const Color(0xFFC8D4D6),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Time asleep',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          '8h 12m',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                        Text(
-                          '10:46 pm - 7:08 am',
-                          style: TextStyle(
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                      ],
-                    ),
+                const Text(
+                  'Your optimal bedtime',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16.0),
-                    color: const Color(0xFFC8D4D6),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Last Week',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          '7h 34m',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                        Text(
-                          '10:46 pm - 7:08 am',
-                          style: TextStyle(
-                            color: Color(0xFF497077),
-                          ),
-                        ),
-                      ],
+                const SizedBox(height: 8),
+                const Text(
+                  '21:30 - 22:30',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    // Handle daily tips and suggestions action
+                  },
+                  child: const Text(
+                    'Daily tips and suggestions →',
+                    style: TextStyle(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
                     ),
                   ),
                 ),
               ],
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavBar(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
-      ),
+        ),
+        const SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  color: const Color(0xFFC8D4D6),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Time asleep',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '8h 12m',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                      Text(
+                        '10:46 pm - 7:08 am',
+                        style: TextStyle(
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  color: const Color(0xFFC8D4D6),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Last Week',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '7h 34m',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                      Text(
+                        '10:46 pm - 7:08 am',
+                        style: TextStyle(
+                          color: Color(0xFF497077),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
